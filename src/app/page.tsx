703d7e5f44a0c5cf25b178dae9df304dd04705a3
MorryNicol <125185639+MorryNicol@users.noreply.github.com>
2025-06-25 23:47:31 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,8 @@ export default function Home() {
           "home",
           "about",
           "job-market-paper",
-          "research",
+          "working-papers",
+          "publish-papers",
           "teaching",
           "contact",
         ];
