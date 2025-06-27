365616366cf7bcc120f68764185cd45f684965f6
MorryNicol <125185639+MorryNicol@users.noreply.github.com>
2025-06-28 00:31:44 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -288,7 +288,7 @@ export default function Home() {
                                   <div className="text-lg font-bold text-gray-800 leading-relaxed">
                                     I am currently on the job market in Marketing.
                                   </div>
-                                  <div className="text-lg text-gray-800 leading-relaxed">
+                                  <div className="text-lg text-gray-600 leading-relaxed">
                                     I am expected to graduate in May 2026.
                                   </div>
                                 </div>
@@ -546,7 +546,7 @@ export default function Home() {
                   <div className="text-lg font-bold text-gray-800 leading-relaxed">
                     I am currently on the job market in Marketing.
                   </div>
-                  <div className="text-lg text-gray-800 leading-relaxed">
+                  <div className="text-lg text-gray-600 leading-relaxed">
                     I am expected to graduate in May 2026.
                   </div>
                 </div>
